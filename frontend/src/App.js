b5af95bfa892210cfedeadf11fa1855df4186f13
Neil Bagchi <65248311+NeilBag@users.noreply.github.com>
2024-07-10 01:47:24 +0530

    API base URL update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import SaveIcon from '@mui/icons-material/Save';
 import EmailIcon from '@mui/icons-material/Email';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://google-search-results-app-2.onrender.com//api';
 
 const SearchResultsApp = () => {
   const [searchQuery, setSearchQuery] = useState('');
