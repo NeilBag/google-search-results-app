5a89e5ba90d16a308dd8a61b69ac26c8b595a9de
Neil Bagchi <65248311+NeilBag@users.noreply.github.com>
2024-07-10 13:33:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -151,6 +151,8 @@ app.post('/api/email', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
